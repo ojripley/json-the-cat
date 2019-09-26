da698a664d8630734173a4ff16a8798ddbdf2b59
Owen Ripley <ojripley@gmail.com>
2019-09-26 10:13:13 -0400

    error testing complete

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,22 +6,19 @@ const fetchBreedDescription = function(searchFor, callback) {
   const searchURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + searchFor;
   request(searchURL, (error, response, body) => {
     if (!error) {
-      if (response.statusCode === 200) {
-        const data = JSON.parse(body);
-        if (data[0] !== undefined) {
-          console.log(data[0].description);
-        } else {
-          console.log('The kitty you requested does not exist in our records!');
-        }
+      const data = JSON.parse(body);
+      if (data[0] !== undefined) {
+        // callback is used in variopus places to handle various scenarios
+        callback(null, data[0].description.trim());
       } else {
-        console.log('Something went wrong!\nResponse status code:', response.statusCode);
+        // made my own error message here since a cat not found does not actually result in one
+        callback('The kitty you requested does not exist in our records!', null);
       }
     } else {
-      console.log(error);
+      callback(error, null);
     }
   });
 };
-
 
 module.exports = { fetchBreedDescription };
 
@@ -29,6 +26,8 @@ module.exports = { fetchBreedDescription };
 
 
 // before refactor
+
+
 // request(searchURL, (error, response, body) => {
 //   if (!error) {
 //     if (response.statusCode === 200) {
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const arg = process.argv[2];
-
 const { fetchBreedDescription } = require('./breedFetcher');
 
+// calls the imported function from breedFetrcher.js
+// passes in the search argument, and a call back that tells the function how to handle either:
+// (1) the description from the requested data
+// (2) an error
 fetchBreedDescription(arg, (error, description) => {
-  if (error) {
-    console.log('Error! Details:\n', error);
-  } else {
+  if (!error) {
     console.log(description);
+  } else {
+    console.log('Error! Details:\n', error);
   }
 });
